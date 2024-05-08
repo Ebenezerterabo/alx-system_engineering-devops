061165e04dcb4bfad6e057dd2aa66a5346de8153
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-05-08 21:50:21 +0000

    Task 1 solved

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install flask using the pip3
 
-package {'flask':
+package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
+}
+
+package { 'werkzeug':
+    ensure    => '2.1.1',
+    provider  => 'pip3',
 }
