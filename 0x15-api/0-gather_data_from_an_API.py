6bb776bd00a69906e2f3704017df6b2d5002a276
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-04 18:00:42 +0000

    Task 0 solved

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" A script to gather data from an API(REST API) """
+
+from sys import argv
+import requests
+
+# get employee id
+emp_id = argv[1]
+# Fetch employee data
+emp_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+emp_response = requests.get(emp_url)
+emp_data = emp_response.json()
+emp_name = emp_data.get("name")
+# Fetch employee tasks(todo)
+todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={emp_id}"
+todos_response = requests.get(todos_url)
+todos_data = todos_response.json()
+# process employee tasks
+total_tasks = len(todos_data)
+completed_tasks = [task for task in todos_data if task.get("completed")]
+number_of_completed_tasks = len(completed_tasks)
+# print ouput
+print(f"Employee {emp_name} is done with tasks\
+      ({number_of_completed_tasks}/{total_tasks}):")
+# print completed tasks titles
+for task in completed_tasks:
+    print("\t" + task.get("title"))
