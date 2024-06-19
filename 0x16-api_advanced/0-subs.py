331c010f39a1f60f2b8dba2859ba2e6b36a912dc
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-19 04:28:48 +0000

    Task 0 solved

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,11 @@ def number_of_subscribers(subreddit):
     """
     A function to get the number of subscribers of a subreddit
     """
-    try:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            subscribers = response.json()["data"]["subscribers"]
-            return subscribers
-        else:
-            return 0
-    except requests.RequestException:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "jeybenz"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        subscribers = response.json()["data"]["subscribers"]
+        return subscribers
+    else:
         return 0
