5e3c712d85695eed9ec1c97d86f047bf466b1023
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-20 15:39:35 +0000

    Task 0 solved

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,18 @@ import requests
 def number_of_subscribers(subreddit):
     """
     A function to get the number of subscribers of a subreddit
+    returns 0 if the subreddit doesn't exits
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "jeybenz"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        subscribers = response.json()["data"]["subscribers"]
-        return subscribers
+        data = response.json()
+        if "data" in data and "subscribers" in data["data"]:
+            return data["data"]["subscribers"]
+        else:
+            return 0
+    elif response.status_code == 404:
+        return 0
     else:
         return 0
