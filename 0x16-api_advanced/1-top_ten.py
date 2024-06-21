f2d7a7632572faaeefd75486c3ca32b891b74a72
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-21 02:05:49 +0000

    Task 1 solved

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,7 +13,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     params = {"limit": 10}
-    response = requests.get(url,headers=headers,
+    response = requests.get(url, headers=headers,
                             params=params, allow_redirects=False)
 
     if response.status_code != 200:
