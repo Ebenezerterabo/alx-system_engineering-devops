e2e10e604c0917b5b83f02b79cda0159fb1e273b
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-21 00:30:18 +0000

    Task 0 solved

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,12 +14,7 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "jeybenz"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 404:
-        return 0
     if response.status_code == 200:
-        data = response.json()
-        if "data" in data and "subscribers" in data.get('data'):
-            return data.get('data').get('subscribers')
-        else:
-            return 0
+        data = response.json().get("data")
+        return data.get("subscribers")
     return 0
