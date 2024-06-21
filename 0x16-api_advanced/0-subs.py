7a61c780a773d253b5318dff9597e1e9d7a0c3e6
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-21 00:39:04 +0000

    Task 0 solved

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     returns 0 if the subreddit doesn't exits
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "jeybenz"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
