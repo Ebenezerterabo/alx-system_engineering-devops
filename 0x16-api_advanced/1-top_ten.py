35bcce4944dccf9283b1af0fdea2d6b958209118
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-21 01:57:17 +0000

    Task 1 solved

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,12 +13,12 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        for i, post in enumerate(data["data"]["children"]):
-            if i < 10:
-                print(post["data"]["title"])
-            else:
-                break
-    else:
+    
+    if response.status_code != 200:
         print(None)
+        return
+    
+        
+    data = response.json().get("data").get("children")
+    for post in data:
+        print(post.get("data").get("title"))
