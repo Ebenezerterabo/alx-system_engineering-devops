3765fc18ac587104939b62cd5b9695710978cc84
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-28 02:07:48 +0000

    Advance task solved

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 exec { 'fix holberton soft':
     command => "sed -i '/holberton soft/s/4/10000/' /etc/security/limits.conf",
-    path    => ['usr/bin', 'usr/local/bin', 'sbin', 'bin'],
+    path    => ['/usr/bin', '/usr/local/bin', '/sbin', '/bin'],
 }
 
 # fixing too many files (holberton hard)
 
 exec { 'fix holberton hard':
     command => "sed -i '/holberton hard/s/5/10000/' /etc/security/limits.conf",
-    path    => ['usr/bin', 'usr/local/bin', 'sbin', 'bin'],
+    path    => ['/usr/bin', '/usr/local/bin', '/sbin', '/bin'],
 }
